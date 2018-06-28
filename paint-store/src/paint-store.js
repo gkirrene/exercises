81b38a085a7b9fe9f6c5cb067b92248b4f1fa274
Gary Kirrene <gkirrene@gmail.com>
2018-06-27 20:09:39 -0700

    WIP

--- paint-store/src/paint-store.js
+++ paint-store/src/paint-store.js
@@ -3,10 +3,10 @@ var PaintStore = (colors = []) => {
 
     return {
         colors: {
-            find:   (color) => console.log('find' + color),
+            count:  () => console.log('count'),
             categories:  () => console.log('categories'),            
             names:  () => console.log('names'),
-            count:  () => console.log('count'),
+            find:   (color) => console.log('find' + color),
             add:    (color) => console.log('add' + color),
             update: (color) => console.log('update' + color),
             remove: (color) => console.log('remove' + color)
